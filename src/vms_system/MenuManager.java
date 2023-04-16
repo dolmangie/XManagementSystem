01689acaae507d95d67e41adaaeb731e1e6d0396
USER <USER@AB350M-DESK>
2023-04-16 20:00:28 +0900

    arraylist add

--- src/vms_system/MenuManager.java
+++ src/vms_system/MenuManager.java
@@ -4,61 +4,38 @@ import java.util.Scanner;
 
 public class MenuManager {
 	public static void main(String[] args) {
-			int num = 0;
-			Scanner input = new Scanner(System.in);
-			while (num != 6) { //Repeatedly print menu
-				System.out.println("1. Add valuables");
-				System.out.println("2. Delete valuables");
-				System.out.println("3. Edit valuables");
-				System.out.println("4. View valuables");
-				System.out.println("5. Show a menu");
-				System.out.println("6. Exit");
-				//Printing Menu about management system
-				System.out.println("Select one number between 1-6: ");
-				num = input.nextInt();
-				//Take number from user
-				if(num == 1){
-					addValuable();
-				}
-				else if (num == 2){
-					deleteValuable();
-				}
-				else if (num == 3){
-					editValuable();
-				}
-				else if (num == 4){
-					viewValuable();
-				}
-				else{
-					continue;
-				}
-				
-			}
-	}
-		 
-	public static void addValuable() {
+		int num = 0;
 		Scanner input = new Scanner(System.in);
-		System.out.print("Valuables name: ");
-		String valuablesName = input.next();
-		System.out.print("Valuables type: ");
-		String valuablesType = input.next();
-		System.out.print("Storage period: ");
-		int valuablesPeriod = input.nextInt();
-	} 	//Take data about valuable name, type and how long will be kept
+		ValuableManager valuableManager = new ValuableManager(input);
 
-	public static void deleteValuable(){
-		Scanner input = new Scanner(System.in);
-		System.out.print("Valuables name: ");
-		String valuablesName = input.next();
-	}
-	public static void editValuable() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Valuables name: ");
-		String valuablesName = input.next();
-	}
-	public static void viewValuable() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Valuables name: ");
-		String valuablesName = input.next();
+		while (num != 5) { //Repeatedly print menu
+			System.out.println("****Valuable Management System menu****");
+			System.out.println("1. Add valuable");
+			System.out.println("2. Delete valuable");
+			System.out.println("3. Edit valuable");
+			System.out.println("4. View valuables");
+			System.out.println("5. Exit");
+			//Printing Menu about management system
+
+			System.out.println("Select one number between 1-5: ");
+			num = input.nextInt();
+			//Take number from user
+
+			if(num==1)
+				valuableManager.addValuable();
+
+			else if(num==2)
+				valuableManager.deleteValuable();
+			
+			else if(num==3)
+				valuableManager.editValuable();
+		
+			else if(num==4)
+				valuableManager.viewValuables();
+			
+			else
+				continue;
+		}
 	}
 }
+	
